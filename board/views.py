b6ef2a9ece906e6c2b3b9dd3dcf7d55c8fde9aad
Jason Thomas <jason@lithiumfox.com>
2014-10-30 18:20:01 -0400

    return last 30 days of events. always.

--- board/views.py
+++ board/views.py
@@ -31,7 +31,6 @@ class IndexView(BoardMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['default'] = Status.objects.default()
-        context['past'] = get_past_days(5)
         return context
 
 
@@ -39,33 +38,12 @@ class ServiceView(BoardMixin, DetailView):
     model = Service
     template_name = 'board/service_detail.html'
 
-    def get(self, request, slug=None, year=None, month=None, day=None):
+    def get(self, request, slug=None, days=30):
 
         data = get_object_or_404(self.model, slug=slug)
 
-        if year:
-            year = int(year)
-        if month:
-            month = int(month)
-        if day:
-            day = int(day)
-
-        if day:
-            start_date = datetime.date(year, month, day)
-            end_date = start_date + datetime.timedelta(days=1)
-        elif month:
-            start_date = datetime.date(year, month, 1)
-            days = calendar.monthrange(start_date.year,
-                                       start_date.month)[1]
-            end_date = start_date + datetime.timedelta(days=days)
-        elif year:
-            leap = 366 if calendar.isleap(year) else 365
-            start_date = datetime.date(year, 1, 1)
-            end_date = start_date + datetime.timedelta(days=leap)
-        else:
-            # get last 30 days
-            start_date = datetime.datetime.now() + datetime.timedelta(days=30)
-            end_date = datetime.datetime.now()
+        start_date = datetime.date.today()
+        end_date = start_date + datetime.timedelta(days=days)
 
         events = data.events.filter(start__gte=start_date).filter(start__lt=end_date)
 
