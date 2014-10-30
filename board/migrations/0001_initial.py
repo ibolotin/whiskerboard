2821f04834837a90b0abe5c5d8979a9f9d9f9194
Jason Thomas <jason@lithiumfox.com>
2014-10-30 09:55:05 -0400

    upgrade to django 1.7.

--- board/migrations/0001_initial.py
+++ board/migrations/0001_initial.py
@@ -1,83 +1,86 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+import colorfield.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
+    dependencies = [
+    ]
 
-        # Adding model 'Service'
-        db.create_table('board_service', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=255)),
-        ))
-        db.send_create_signal('board', ['Service'])
-
-        # Adding model 'Status'
-        db.create_table('board_status', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('severity', self.gf('django.db.models.fields.IntegerField')()),
-            ('image', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('color', self.gf('colorfield.fields.ColorField')()),
-        ))
-        db.send_create_signal('board', ['Status'])
-
-        # Adding model 'Event'
-        db.create_table('board_event', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('service', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['board.Service'])),
-            ('status', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['board.Status'])),
-            ('message', self.gf('django.db.models.fields.TextField')()),
-            ('start', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
-            ('informational', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal('board', ['Event'])
-
-    def backwards(self, orm):
-
-        # Deleting model 'Service'
-        db.delete_table('board_service')
-
-        # Deleting model 'Status'
-        db.delete_table('board_status')
-
-        # Deleting model 'Event'
-        db.delete_table('board_event')
-
-    models = {
-        'board.event': {
-            'Meta': {'ordering': "('-start',)", 'object_name': 'Event'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'informational': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'message': ('django.db.models.fields.TextField', [], {}),
-            'service': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['board.Service']"}),
-            'start': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'status': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['board.Status']"})
-        },
-        'board.service': {
-            'Meta': {'object_name': 'Service'},
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
-        },
-        'board.status': {
-            'Meta': {'object_name': 'Status'},
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'severity': ('django.db.models.fields.IntegerField', [], {}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'})
-        }
-    }
-
-    complete_apps = ['board']
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('slug', models.SlugField()),
+                ('description', models.CharField(max_length=255)),
+            ],
+            options={
+                'ordering': ('name',),
+                'verbose_name_plural': 'categories',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Event',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('message', models.TextField()),
+                ('start', models.DateTimeField(default=datetime.datetime.now)),
+                ('informational', models.BooleanField(default=False)),
+            ],
+            options={
+                'ordering': ('-start',),
+                'get_latest_by': 'start',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Service',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('slug', models.SlugField()),
+                ('description', models.CharField(max_length=255)),
+                ('category', models.ForeignKey(related_name='services', to='board.Category', null=True)),
+            ],
+            options={
+                'ordering': ('name',),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Status',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('slug', models.SlugField()),
+                ('description', models.CharField(max_length=255)),
+                ('severity', models.IntegerField(choices=[(10, b'NORMAL'), (30, b'WARNING'), (40, b'ERROR'), (50, b'CRITICAL')])),
+                ('image', models.CharField(max_length=100)),
+                ('color', colorfield.fields.ColorField(max_length=10)),
+            ],
+            options={
+                'ordering': ('severity',),
+                'verbose_name_plural': 'statuses',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='service',
+            field=models.ForeignKey(related_name='events', to='board.Service'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='event',
+            name='status',
+            field=models.ForeignKey(related_name='events', to='board.Status'),
+            preserve_default=True,
+        ),
+    ]
--- board/settings.py
+++ board/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
     'board',
     'colorfield',
     'tastypie'
